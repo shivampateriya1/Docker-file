4fbf8df6a512fb0d6ef636d1c734031719051818
shivpateriya <shivampateriya40@gmail.com>
2022-11-30 01:04:28 +0530

    first commit

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
 COPY . .
-
+EXPOSE 3000
 CMD [ "python3","-m","flask","run","--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -2,5 +2,7 @@ from flask import Flask,render_template
 p1 = Flask(__name__)
 
 @p1.route('/')
-def assigment():
+def main():
     return render_template('try.html')
+if __name__ == '__main__':
+    p1.run(host="0.0.0.0",port=8082)
